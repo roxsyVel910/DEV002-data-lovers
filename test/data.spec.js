5ca23f64ab03dbd0936dd5e8e475db6d70b6a597
Roxsy <roxsy.dj@gmail.com>
2022-12-10 17:32:41 -0500

    tratando de arreglar test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,16 +1,15 @@
 
 import {filterData } from '../src/data.js';
 
+const arrExpected = [{'director': 'fer' }, {'director': 'fer'}, {'director': 'fer'}];
+
+    const arrToTest = [{'director': 'fer' }, {'director': 'fer'}, {'director': 'fer'}, {'director': 'nico'}, {'director': 'rox'}];
+
 describe('filterData', ()=> {
 
   it('Deberia retornar 2 si sus argumentos son 1 y 1', ()=> {
 
-    const arrExpected = [{'director': 'fer' }, {'director': 'fer'}, {'director': 'fer'}];
-
-    const arrToTest = [{'director': 'fer' }, {'director': 'fer'}, {'director': 'fer'}, {'director': 'nico'}, {'director': 'rox'}];
-
     const resultado = filterData(arrToTest, 'fer', 'director');
-
     expect(resultado).toEqual(arrExpected);
   })
 
